ccf5146a40731276d3b0ce9f297a2ceb2ad1f9d2
shivamdargan <darganshivam2001@gmail.com>
2021-05-15 23:52:13 +0530

    Added basic client side folder structure
    Used create-react-app to create the frontend folder structure and basic file

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home/index'
+  
+function App() {
+  return (
+    <div className="App">
+      
+      <Home />
+    </div>
+  );
+}
+
+export default App;
